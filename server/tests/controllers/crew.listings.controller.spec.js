15794043434ad2f9e7e8a5b0ac3eb6cae7a4162c
adam-s <adamsohn1@gmail.com>
2016-08-22 21:09:18 -0400

    Admin update crew profile

--- server/tests/controllers/crew.listings.controller.spec.js
+++ server/tests/controllers/crew.listings.controller.spec.js
@@ -76,10 +76,65 @@ describe.only('/api/crew-listings', function() {
                 .field('author', '57bb59aa513d0e0f4f72ef9b')
                 .attach('file', __dirname + '/../fixtures/test.txt')
                 .end(function(err, response) {
-                    console.log(response.body);
                     expect(response.status).to.equal(200);
                     done();
                 })
+        });
+
+        it('should update a profile', function(done) {
+            agent
+                .post('/api/crew-listings')
+                .field('startDate', '1470017218561')
+                .field('checkIn', '1470017218561')
+                .field('title', 'consequatur dolorem facere voluptatem debitis')
+                .field('description', 'Quibusdam laudantium pariatur labore qui consequatur incidunt. Voluptatem quia laudantium. Assumenda quia labore veritatis eius aliquam et. Veritatis debitis quos quia sequi perspiciatis dolor est natus soluta. Ullam ipsam consequatur quaerat ipsa omnis. Nostrum necessitatibus perspiciatis sequi adipisci error.')
+                .field('phone', '934-182-8580')
+                .field('email', 'Sabrina.OKon@hotmail.com')
+                .field('position', 'Dayworker')
+                .field('languages[0]', 'Russian')
+                .field('active', 'true')
+                .field('location[name]', 'ut nulla occaecati')
+                .field('location[locality]', 'Haagmouth')
+                .field('location[administrativeArea', 'Kentucky')
+                .field('location[country]', 'Turkmenistan')
+                .field('location[coordinates][0]', '-0.6660')
+                .field('location[coordinates][1]', '38.3562')
+                .field('smoking', 'false')
+                .field('papers', 'false')
+                .field('jobType', 'Commercial')
+                .field('name', 'Bridgette Hahn')
+                .field('author', '57bb59aa513d0e0f4f72ef9b')
+                .attach('file', __dirname + '/../fixtures/test.txt')
+                .end(function() {
+                    agent
+                        .post('/api/crew-listings')
+                        .field('startDate', '1470017218561')
+                        .field('checkIn', '1470017218561')
+                        .field('title', 'consequatur dolorem facere voluptatem debitis')
+                        .field('description', 'Quibusdam laudantium pariatur labore qui consequatur incidunt. Voluptatem quia laudantium. Assumenda quia labore veritatis eius aliquam et. Veritatis debitis quos quia sequi perspiciatis dolor est natus soluta. Ullam ipsam consequatur quaerat ipsa omnis. Nostrum necessitatibus perspiciatis sequi adipisci error.')
+                        .field('phone', '934-182-8580')
+                        .field('email', 'Sabrina.OKon@hotmail.com')
+                        .field('position', 'Dayworker')
+                        .field('languages[0]', 'Russian')
+                        .field('active', 'true')
+                        .field('location[name]', 'ut nulla occaecati')
+                        .field('location[locality]', 'Haagmouth')
+                        .field('location[administrativeArea', 'Kentucky')
+                        .field('location[country]', 'Turkmenistan')
+                        .field('location[coordinates][0]', '-0.6660')
+                        .field('location[coordinates][1]', '38.3562')
+                        .field('smoking', 'false')
+                        .field('papers', 'false')
+                        .field('jobType', 'Commercial')
+                        .field('name', 'Bridgette Hahn')
+                        .field('author', '57bb59aa513d0e0f4f72ef9b')
+                        .attach('file', __dirname + '/../fixtures/test.odt')
+                        .end(function(err, response) {
+                            console.log(response.body);
+                            expect(response.status).to.equal(200);
+                            done();
+                        });
+                });
         });
     });
 });
