60af80b7b377e1825a5d6174565189df7e60d29f
adam-s <adamsohn1@gmail.com>
2016-10-10 06:52:03 -0400

    Fixed root user weirdness

--- server/config/init.js
+++ server/config/init.js
@@ -4,6 +4,9 @@ var config = require('./config');
 
 module.exports = function(app, db) {
     return new Promise(function(resolve) {
+        // TODO I really need to find a much better way to insure that there is an immutable admin user.
+        // The only thing that should be able to change here is the password.
+
         var User = db.model('User');
         User.findWithDeleted({ email: config.admin.accountEmail}, function(err, users) {
             var user = users[0];
@@ -23,6 +26,15 @@ module.exports = function(app, db) {
                 user.restore(function() {
                     return resolve();
                 })
+            } else if (!user.isAdmin()) {
+                //OMG this is so stupid there must be a better way
+                User.update(
+                    { _id: user._id },
+                    { $push: { roles: {$each: ['administrator', 'authenticated', 'anonymous'] }}},
+                    { upsert: true},
+                    function() {
+                        return resolve();
+                    })
             } else {
                 return resolve();
             }
