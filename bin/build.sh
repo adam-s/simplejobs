bb651b1f6197de4379b7b2ce1de4f666973da46c
adam-s <adamsohn1@gmail.com>
2016-10-11 09:22:55 -0400

    Created shell build script

--- bin/build.sh
+++ bin/build.sh
@@ -1,8 +1,31 @@
 #!/bin/bash
 
+# Move config files outside the scope of git which doesn't track them
+cp -rf server/config/environment ~/WebstormProjects/config
+
+# Make sure we are working with the master branch
+git checkout master
+
+# Push latest commit to GitHub
 git add .
-echo "Commit message"
-
-read message
-
+printf "Commit message: "
+read -r message
 git commit -m "$message"
+git push origin master
+
+# Move to staging local
+git checkout staging
+
+# Merge staging with master
+git merge master -m "Preparing to build"
+
+# Build the production files
+grunt build
+git add .
+git commit -m "built"
+
+# Push to remote staging
+git push staging staging:master
+
+# Return to the master branch
+git checkout master
---
+++ bin/move-config.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+cp -rf server/config/environment ~/WebstormProjects/config
